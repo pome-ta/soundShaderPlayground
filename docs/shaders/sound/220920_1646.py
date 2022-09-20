3aee0270bdec507ed66ba8869baefc7414d82df6
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-09-20 16:47:30 +0900

    sound shader

--- docs/shaders/sound/220920_1646.py
+++ docs/shaders/sound/220920_1646.py
@@ -1,4 +1,5 @@
 /* 時報っぽい */
+
 const float BPM = 60.0;
 const float PI = acos(-1.0);
 const float PI2 = PI * 2.0;
@@ -15,9 +16,11 @@ float sine(float freq, float time) {
 vec2 mainSound(float time) {
   float beat = timeToBeat(time);
   
+  float w88 = sin(PI2 * 880.0 * time) * fract(time / -4.0);
+  
   float freq = mod(beat, 4.0) <= 3.0 ? 440.0 : 880.0;
   float amp = exp(-6.0 * fract(beat));
-  
   return vec2(0.4 * (sine(freq, time) * amp));
+  //return vec2(w44 / amp);
 }
 
