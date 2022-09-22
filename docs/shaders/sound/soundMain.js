6d37c4108583aa4eae37368f1ecb7d1a1f0d9344
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-09-22 09:53:24 +0900

    shader

--- docs/shaders/sound/soundMain.js
+++ docs/shaders/sound/soundMain.js
@@ -1,5 +1,5 @@
 // memo: 
-#define BPM 90.0
+#define BPM 120.0
 
 const float PI = acos(-1.0);
 const float TAU = PI * 2.0;
@@ -15,6 +15,34 @@ float pitch(float p) { return pow(2.0, p / 12.0) * 440.0; }
 vec2 mainSound(float time){
   float bpm = timeToBeat(time);
 
-  return vec2(sine(440.0 * time) * exp(-3.0 * time));
+  return vec2(sine(440.0 * time) * fract(-bpm * 1.0 / 4.0));
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
