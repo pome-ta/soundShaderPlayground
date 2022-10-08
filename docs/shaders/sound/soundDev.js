b44857efe3f4dc21038e43d1e567994136e51634
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-10-09 02:16:38 +0900

    up

--- docs/shaders/sound/soundDev.js
+++ docs/shaders/sound/soundDev.js
@@ -2,9 +2,23 @@
 
 const float PI = acos(-1.0);
 const float TAU = PI * 2.0;
+float sine(float phase) {return sin(TAU * phase );}
+
+/* GLSL */
+// 2D Random
+float _rnd(in vec2 st) {
+  return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
+}
+
+float random(float t) {
+  float rnd_x = sine(123.4 * t);
+  float rnd_y = sine(567.8 * t);
+  return _rnd(vec2(rnd_x, rnd_y)) - 0.5;
+}
 
 vec2 mainSound(float time){
-  float wave = sin(TAU * 440.0 * time);
+  // float wave = sin(TAU * 440.0 * time);
+  float wave = random(time);
   if (wave > 0.0) {
     return vec2(wave);
   } else {
