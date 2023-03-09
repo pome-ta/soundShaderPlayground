3e455ee4f5f5c2c46b30de3e59f5593f8228112b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-03-10 02:26:56 +0900

    up

--- docs/shaders/sound/soundDev.js
+++ docs/shaders/sound/soundDev.js
@@ -1,6 +1,24 @@
-// memo: random hash 確認
 precision highp float;
 precision highp int;
+
+#define BPM 90.0
+
+const float PI = acos(-1.0);
+const float TAU = PI * 2.0;
+
+float sine(float phase) { return sin(TAU * phase); }
+/* GLSL */
+// 2D Random
+float _rnd(in vec2 st) {
+  return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
+}
+
+float random(float t) {
+  float rnd_x = sine(123.4 * t);
+  float rnd_y = sine(567.8 * t);
+  return _rnd(vec2(rnd_x, rnd_y)) - 0.5;
+}
+
 
 uint k = 0x456789abu;  // 算術積に使う大きな桁数の定数
 const uint UINT_MAX = 0xffffffffu;  // 符号なし整数の最大値
@@ -19,24 +37,14 @@ float hash11(float p) {
   return float(uhash11(n)) / float(UINT_MAX);  // 値の正規化
 }
 
-const float PI = acos(-1.0);
-const float TAU = PI * 2.0;
-float sine(float phase) {return sin(TAU * phase );}
-
-/* GLSL */
-// 2D Random
-float _rnd(in vec2 st) {
-  return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
-}
-
-float random(float t) {
-  float rnd_x = sine(123.4 * t);
-  float rnd_y = sine(567.8 * t);
-  return _rnd(vec2(rnd_x, rnd_y)) - 0.5;
-}
 
 vec2 mainSound(float time){
-  float hash_l = hash11(time);
-  float random_r = random(time);
-  return vec2(hash_l, random_r);
+  float old_random = random(time);
+  //float hash_random = hash11(time);
+
+  float sound_out = old_random;
+  //float sound_out = hash_random;
+
+  return vec2(sound_out);
 }
+
