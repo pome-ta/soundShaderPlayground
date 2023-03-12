708658c37a9bf996df04f80b441da899052ebf32
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-03-12 15:36:22 +0900

    if

--- docs/shaders/sound/soundDev.js
+++ docs/shaders/sound/soundDev.js
@@ -1,4 +1,4 @@
-precision highp float;
+//precision highp float;
 precision highp int;
 
 // memo: 
@@ -15,12 +15,16 @@ float beatToTime(float b) {return b / BPM * 60.0;}
 float sine(float p) { return sin(TAU * p); }
 float pitch(float p) { return pow(2.0, p / 12.0) * 440.0; }
 
-vec2 mainSound(float time) {
+vec2 mainSound(const float time) {
   float bpm = timeToBeat(time);
   float p;
-  bool l = true;
+  bool swtch = true;
+  if (swtch) {
+    
+  }
+  vec2 loopng = vec2(-1.0, 1.0);
   p = sin(bpm);
-  p -= fract(-bpm);
+  //p -= fract(-bpm);
   float sound = sine(pitch(p) * time);
 
   return vec2(sound);
