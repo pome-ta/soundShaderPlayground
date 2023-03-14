7f843dcace5be4988f662a7e57b17a1e40ba19be
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-03-15 01:18:30 +0900

    up

--- docs/shaders/sound/soundDev.js
+++ docs/shaders/sound/soundDev.js
@@ -19,6 +19,8 @@ float pitch(float p) { return pow(2.0, p / 12.0) * 440.0; }
 vec2 mainSound( float time ) {
   float bpm = timeToBeat(time);
   //float mul_freq = pitch(floor(mod(bpm, 2.0)));
+  float upDown = 
+  float mul_freq = sin(bpm * PI)
   float f4 = pitch(0.0);
   float f8 = pitch(1.0);
   float freq = mix(f4, f8, (mod(bpm, sin(bpm))));
