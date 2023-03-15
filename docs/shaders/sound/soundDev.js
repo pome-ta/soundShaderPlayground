9b06a99d21aaad84818d9c9d1a75869e8345833f
pome-ta <pometarou93@gmail.com>
2023-03-15 22:12:57 +0900

    format

--- docs/shaders/sound/soundDev.js
+++ docs/shaders/sound/soundDev.js
@@ -1,4 +1,4 @@
-precision highp float;  /* 既に内部で呼んでるけど */
+precision highp float; /* 既に内部で呼んでるけど */
 precision highp int;
 
 // memo:
@@ -8,27 +8,26 @@ const float PI = acos(-1.0);
 const float TAU = PI * 2.0;
 
 /* sound common */
-float timeToBeat(float t) {return t / 60.0 * BPM;}
-float beatToTime(float b) {return b / BPM * 60.0;}
-
+float timeToBeat(float t) {return t / 60.0 * BPM; }
+float beatToTime(float b) {return b / BPM * 60.0; }
 
 float sine(float p) { return sin(TAU * p); }
 float pitch(float p) { return pow(2.0, p / 12.0) * 440.0; }
 
-
-vec2 mainSound( float time ) {
+vec2 mainSound(float time) {
   float bpm = timeToBeat(time);
   float tempo = sine((mod(bpm, 4.0) >= 1.0 ? 440.0 : 880.0) * time) *
-exp(-1e2 * fract(bpm));
-
+  exp(-1e2 * fract(bpm));
+  
   float base_freq = pitch(-64.0);
   float p = -sin(dot(bpm, PI));
-
-  float move_freq = pitch( abs(p)) ;
+  
+  float move_freq = pitch(abs(p));
   //float move_freq = pitch(asin(cos(bpm * PI)));
   float wave_tone = sine(base_freq * time + move_freq) * exp(-2.0 * fract(bpm));
   float smpl_tone = sine(440.0 * time);
-
+  
   return vec2(wave_tone);
   // return vec2(wave_tone, smpl_tone + tempo);
 }
+
