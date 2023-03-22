1af1515cda0f4dead236a14f5c880c493ce82d20
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-03-22 23:22:15 +0900

    attack

--- docs/shaders/sound/soundDev.js
+++ docs/shaders/sound/soundDev.js
@@ -2,7 +2,7 @@ precision highp int;
 
 
 // memo:
-#define BPM 90.0
+#define BPM 110.0
 const float PI = acos(-1.0);
 const float TAU = (PI * 2.0);
 
@@ -19,7 +19,8 @@ float bassDrum(in float t) {
   //float phase = 32.0 * t - 24.0 * exp(-1.25 * t);
   float phase = 24.0 * exp(-0.75 * t);
   // float attack = sine(phase) * amp;
-  float attack = sine(phase);
+  //float attack = sine(phase);
+  float attack = cos(TAU * 24.0 * exp(-0.75 * t)) * pow(fract(-t), 16.0);
   bdMaster += attack;
   
   return bdMaster;
@@ -31,13 +32,15 @@ vec2 mainSound(float time) {
   float tempo = sine((mod(bpm, 4.0) >= 1.0 ? 440.0 : 880.0) * time) *
   exp(-1e2 * fract(bpm));
   float outSound;
-  float bdTone = pitch(0.0);
   
-  float kikTiming = mod(bpm, 1.0);
+  float kikTiming = mod(bpm, 8.0) < 7.0 ? mod(bpm, 1.0) : mod(bpm, 0.5);
+  
+  float sineTone = sine(pitch(0.0) * time) * kikTiming;
   float bd = bassDrum(kikTiming);
   
   outSound += bd;
-  //outSound += tempo;
+  //outSound += sineTone;
+  // outSound += tempo;
   //outSound = outSound * 0.8;
   
   return vec2(outSound);
