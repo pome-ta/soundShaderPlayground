2a56bf1201d57e6990c4f8ae3ee28e117a27ba31
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-03-23 16:40:06 +0900

    up

--- docs/shaders/sound/soundDev.js
+++ docs/shaders/sound/soundDev.js
@@ -14,33 +14,45 @@ float sine(float p) { return sin(TAU * p); }
 float pitch(float p) { return pow(2.0, p / 12.0) * 440.0; }
 
 float bassDrum(in float t) {
- float bdMaster;
- float attack = cos(TAU * 24.0 * exp(-0.75 * t)) * pow(fract(-t), 16.0);
- float body = sine(32.0 * t - pitch(abs(tan(t / 3.0)))) * exp(-2.0 * fract(t));
-
- bdMaster += attack * 0.4;
- bdMaster += body * 0.64;
- bdMaster += mix(attack, body, t) * 0.5;
-
- return bdMaster;
+  float bdMaster;
+  
+  float attack = cos(TAU * 24.0 * exp(-0.75 * t)) * pow(fract(-t), 16.0);
+  
+  float p = sin(t * PI) / 4.0;
+  
+  float moveFreq = pitch( (p) * 0.5);
+  
+  float body = sine(32.0 * t + moveFreq) * fract(-t-2.0);
+  
+  bdMaster += attack * 0.4;
+  bdMaster += body * 0.64;
+  bdMaster += mix(attack, body, t) * 0.5;
+  
+  return bdMaster;
 
 }
 
 vec2 mainSound(float time) {
- float bpm = timeToBeat(time);
- float tempo = sine((mod(bpm, 4.0) >= 1.0 ? 440.0 : 880.0) * time) *
-exp(-1e2 * fract(bpm));
- float outSound;
+  float bpm = timeToBeat(time);
+  float tempo = sine((mod(bpm, 4.0) >= 1.0 ? 440.0 : 880.0) * time) * exp(-1e2 * fract(bpm));
+  
+  float outSound;
 
- float kikTiming = mod(bpm, 16.0) <= 15.0 ? mod(bpm, 1.0) : mod(bpm, 0.5);
+  float kikTiming = mod(bpm, 16.0) <= 15.0 ? mod(bpm, 1.0) : mod(bpm, 0.5);
 
- float sineTone = sine(pitch(0.0) * time ) * kikTiming;
- float bd = bassDrum(kikTiming);
+  float sineTone = sine(pitch(0.0) * time * mix(0.0, 1.0,fract(-bpm)));
+  float bd = bassDrum(kikTiming);
+  float base_freq = pitch(0.0);
+  float p = sin(bpm * PI) * 4.0;
+  
+  float move_freq = pitch( (p) * 0.5 + 0.5 );
+  float wave_tone = sine(base_freq * time + move_freq);
+  float smpl_tone = sine(440.0 * time);
 
- outSound += bd;
- //outSound += sineTone;
- // outSound += tempo;
- //outSound = outSound * 0.8;
-
- return vec2(outSound);
+  outSound += bd;
+  //outSound += wave_tone;
+  // outSound += tempo;
+  //outSound = outSound * 0.8;
+  
+  return vec2(outSound);
 }
